2f879aa6404abed3d00c1977ab4e5fecc531035a
Rushikesh Patil <rushikpatil1@gmail.com>
2023-12-11 01:34:48 +0530

    Car-Companion

---
+++ app/src/main/java/com/example/carcompanion/Login.java
@@ -0,0 +1,164 @@
+package com.example.carcompanion;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Pair;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.*;
+
+public class Login extends AppCompatActivity {
+    Button callSignUp,login_btn;
+    ImageView image;
+    TextView logoText,sloganText;
+    TextInputLayout username,password;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_login);
+
+        //hooks
+        callSignUp = findViewById(R.id.signup_screen);
+        image = findViewById(R.id.Logo_Image);
+        logoText = findViewById(R.id.Logo_name);
+        sloganText = findViewById(R.id.slogan_name);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login_btn = findViewById(R.id.Login_btn);
+
+        callSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login.this,SignUp.class);
+                //startActivity(intent);
+                Pair[] pairs = new Pair[7];
+                pairs[0] = new Pair<View,String>(image, "logo_image");
+                pairs[1] = new Pair<View,String>(logoText, "logo_text");
+                pairs[2] = new Pair<View,String>(sloganText, "logo_desc");
+                pairs[3] = new Pair<View,String>(username, "username_tran");
+                pairs[4] = new Pair<View,String>(password, "password_tran");
+                pairs[5] = new Pair<View,String>(login_btn, "button_tran");
+                pairs[6] = new Pair<View,String>(callSignUp, "login_signup_tran");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(Login.this,pairs);
+                // options = ActivityOptions.makeSceneTransitionAnimation(MainActivity.this,pairs);
+                startActivity(intent,options.toBundle());
+            }
+        });
+
+
+
+
+
+
+    }
+
+    private Boolean validateUsername(){
+        String val = username.getEditText().getText().toString();
+        if(val.isEmpty()){
+            username.setError("Field cannot be empty");
+            return false;
+        } else{
+            username.setError(null);
+            username.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    private Boolean validatePassword(){
+        String val = password.getEditText().getText().toString();
+
+        if(val.isEmpty()){
+            password.setError("Field cannot be empty");
+            return false;
+        } else{
+            password.setError(null);
+            password.setErrorEnabled(false);
+            return true;
+        }
+    }
+
+    public void loginUser(View view){
+        if(!validateUsername() | !validatePassword()){
+            return;
+        }else{
+            isUser();
+        }
+    }
+
+    private void isUser() {
+        final String userEnteredUsername = Objects.requireNonNull(username.getEditText()).getText().toString().trim();
+        final String userEnteredPassword = Objects.requireNonNull(password.getEditText()).getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("users");
+
+        Query checkUser = reference.orderByChild("username").equalTo(userEnteredUsername);
+
+        checkUser.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()){
+
+                    username.setError(null);
+                    username.setErrorEnabled(false);
+
+                    String passwordFromDB = snapshot.child(userEnteredUsername).child("password").getValue(String.class);
+
+                    if(passwordFromDB.equals(userEnteredPassword)){
+
+                        username.setError(null);
+                        username.setErrorEnabled(false);
+
+                        String nameFromDB = snapshot.child(userEnteredUsername).child("name").getValue(String.class);
+                        String usernameFromDB = snapshot.child(userEnteredUsername).child("username").getValue(String.class);
+                        String phoneNoFromDB = snapshot.child(userEnteredUsername).child("phoneNo").getValue(String.class);
+                        String emailFromDB = snapshot.child(userEnteredUsername).child("email").getValue(String.class);
+
+                        Intent intent = new Intent(getApplicationContext(),UserProfile.class);
+
+                        intent.putExtra("name",nameFromDB);
+                        intent.putExtra("username",usernameFromDB);
+                        intent.putExtra("email",emailFromDB);
+                        intent.putExtra("phoneNo",phoneNoFromDB);
+                        intent.putExtra("password",passwordFromDB);
+
+                        startActivity(intent);
+                    }
+                    else {
+                        password.setError("Wrong Password");
+                        password.requestFocus();
+                    }
+                }
+                else {
+                    username.setError("No such User exist");
+                    username.requestFocus();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
